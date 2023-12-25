24f67719d998741bb4b00bcf982d89c02274b0df
Francesco Pio Cellamare <s309530@studenti.polito.it>
2023-12-25 10:34:32 +0100

    toggle more togglish now

--- client/TRACKIT/components/ToggleChoice.jsx
+++ client/TRACKIT/components/ToggleChoice.jsx
@@ -17,10 +17,10 @@ const ToggleChoice = () => {
 
   return (
     <View onValueChange={value => setValue(value)} value={value} style={styles.container}>
-        <Button icon="calendar" buttonColor={ value == 'left' ? 'blue' : 'grey' } mode="contained" style={styles.buttonStyle} onPress={() => handleToggle('left')}>
+        <Button icon="calendar" buttonColor={ value == 'left' ? 'blue' : 'grey' } mode="contained" style={styles.buttonLeftStyle} onPress={() => handleToggle('left')}>
           By Time
         </Button>
-        <Button icon="car" buttonColor={ value == 'left' ? 'gray' : 'blue' } mode="contained" style={styles.buttonStyle} onPress={() => handleToggle('right')}>
+        <Button icon="car" buttonColor={ value == 'left' ? 'gray' : 'blue' } mode="contained" style={styles.buttonRightStyle} onPress={() => handleToggle('right')}>
           By Driver
         </Button>
     </View>
@@ -31,8 +31,15 @@ const styles = StyleSheet.create({
       flexDirection: 'row',
       alignItems: 'center'
     },
-    buttonStyle: {
-        borderRadius: 10
+    buttonLeftStyle: {
+        borderRadius: 0,
+        borderTopLeftRadius: 10,
+        borderBottomLeftRadius: 10
+    },
+    buttonRightStyle: {
+        borderRadius: 0,
+        borderTopRightRadius: 10,
+        borderBottomRightRadius: 10
     }
   });
 export default ToggleChoice;
