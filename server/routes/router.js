a3ead4bedd4aecfa5c7537a5d4a695452903a75c
Francesco Pio Cellamare <s309530@studenti.polito.it>
2024-01-15 18:15:09 +0100

    routes to do

--- server/routes/router.js
+++ server/routes/router.js
@@ -5,7 +5,29 @@ const express = require('express');
 
 const router = express.Router();
 
+router.post('/login')
+router.delete('/logout')
+router.get('/loggedin')
 
+// fake registration
+router.post('/register')
+
+// search a ride
 router.get('/rides', ridesController.searchRide);
+
+// book a practice for the current student
+router.put('/rides')
+
+// request by the current student
+router.post('/rides')
+
+// get all topics
+router.get('/topic')
+
+// get all evaluations for the student
+router.get('/evaluations')
+
+// delete a notification for a student
+router.delete('/notification/:id')
 
 module.exports = router;
